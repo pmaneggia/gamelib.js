44fc0260776db8e4ac84d65b9592d384f2ba9355
Julian Dorn <post@wi-wissen.de>
2018-07-15 12:37:15 +0200

    Initial commit

---
+++ js/gamelib.js
@@ -0,0 +1,375 @@
+class World {
+
+    constructor(domelement, width, height, background = null, useGrid = false, showGrid = false, gridSize = 0) {
+        this.canvas = document.createElement('canvas');
+        this.canvas.id = "world";
+        this.canvas.width = width;
+        this.canvas.height = height;
+
+        this.ctx = this.canvas.getContext("2d");
+
+        this.background = background;
+        this.useGrid = useGrid;
+        this.showGrid = showGrid;
+        this.gridSize = gridSize;
+        this._entity = [];
+        this.intervalID = undefined;
+        this._keyDown = [];
+        this._keyUp = [];
+
+        //get into dom
+        var domentry = document.getElementsByTagName(domelement)[0];
+        domentry.appendChild(this.canvas);
+
+        //for loading images with correct path
+        var scripts = document.getElementsByTagName('script');
+        var path = scripts[scripts.length - 1].src.split('?')[0];      // remove any ?query
+        this.basePath = path.split('/').slice(0, -1).join('/') + '/';  // remove last filename part of path
+
+        //get all keyboard inputs
+        var self = this;
+        document.body.addEventListener("keydown", function (event) {
+            if (event.defaultPrevented) {
+                return; // Do nothing if the event was already processed
+            }
+
+            if (!self._keyDown.includes(event.key)) self._keyDown.push(event.key);
+            
+            // Cancel the default action to avoid it being handled twice
+            event.preventDefault();
+        }, true);
+
+        document.body.addEventListener("keyup", function (event) {
+            if (event.defaultPrevented) {
+                return; // Do nothing if the event was already processed
+            }
+
+            if (!self._keyUp.includes(event.key)) self._keyUp.push(event.key);
+            // Cancel the default action to avoid it being handled twice
+            event.preventDefault();
+        }, true);
+    }
+
+    async play() {
+        var self = this;
+        function update() {
+            self.update();
+        }
+
+        if (this.intervalID) stop();
+        await this.preload();
+        this.intervalID = window.setInterval(update, 16);
+    }
+
+    stop() {
+        clearInterval(this.intervalID);
+    }
+
+    get gridHeight() {
+        return Math.floor(this.height() / this.gridSize);
+    }
+
+    get gridWidth() {
+        return Math.floor(this.width() / this.gridSize);
+    }
+
+    getEntity(name) {
+        return this._entity[name];
+    }
+
+    removeEntity(e) {
+        for (let i = 0; i < this._entity.length; ++i) {
+            if (this._entity[i] == e) this._entity.splice(i, 1);
+        }
+    }
+
+    checkCollision(a, x, y) {
+        var result = false;
+        for (let b of this._entity) {
+            if (a != b) {
+                if (!(x >= b.x + b.width || y >= b.y + b.height ||
+                    x + a.width <= b.x || y + a.height <= b.y)) {
+                        //console.log(a.collidate(b) || b.collidate(a));
+                    result = result || (a.collidate(b) || b.collidate(a));
+                }
+            }
+        }
+        return result;
+    }
+
+    removeAllEntities() {
+        this._entity = [];
+    }
+
+    addEntity(entity) {
+        entity.world = this;
+        this._entity.push(entity);
+    }
+
+    async preload() {
+        var n = 0;
+        var loaded = 0;
+        var self = this;
+        //count assets
+        for (let e of this._entity) {
+            if (e.img.hasOwnProperty("url")) n++;
+        }
+        if (this.background.hasOwnProperty("url")) n++;
+
+        var loadImage = function (src) {
+            var img = new Image();
+            img.onload = complete;
+            console.log("img.src = " + self.basePath + "../" + src);
+            img.src = self.basePath + "../" + src;
+            return img;
+        }
+
+        var complete = function () {
+            loaded += 1;
+            if (loaded === n) {
+                console.log("prealoding compleated");
+                return true;
+            }
+        }
+
+        //preload all needed assets
+        console.log('starting preload');
+        for (let e of this._entity) {
+            if (e.img.hasOwnProperty("url")) {
+                e.img.img = loadImage(e.img.url);
+            }
+        }
+        if (this.background.hasOwnProperty("url")) this.background.img = loadImage(this.background.url);
+    }
+
+    update() {
+        //key events
+        for (let entry of this._keyDown) {
+            //call listener for hold
+            for (let e of this._entity) {
+                e.keyHold(entry);
+            }
+        }
+
+        for (var i = 0; i < this._keyUp.length; i++) {
+            //call listener for click
+            for (let e of this._entity) {
+                e.keyPressed(this._keyUp[i]);
+            }
+
+            if (this._keyDown.indexOf(this._keyUp[i]) != -1) {
+                this._keyDown.splice(this._keyDown.indexOf(this._keyUp[i]), 1);
+            }
+            this._keyUp.splice(i, 1);
+        }
+
+        //update all
+        for (let e of this._entity) {
+            e.update();
+        }
+
+        //show result
+        this.draw();
+    }
+
+    draw() {
+        //draw background
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        if (this.background) {
+            this.background.draw(this.ctx, 0, 0);
+        } else {
+            this.ctx.fillStyle = "white";
+            this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        }
+
+        //draw entities
+        for (let e of this._entity) {
+            e.draw(this.ctx);
+        }
+
+        return this.canvas;
+
+    }
+
+}
+
+class Entity {
+    constructor(x, y, width, height, type = "entity") {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.type = type;
+    }
+
+    draw() {
+        throw Error;
+    }
+
+    move(x, y) {
+        if (!this.world.checkCollision(this, this.x + x, this.y + y)) {
+            this.x = this.x + x;
+            this.y = this.y + y;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async step(x, y) {
+        if (this.world.checkCollision(this, this.x + x * this.world.gridSize, this.y + y * this.world.gridSize)) {
+            for (i = 0; i < x * this.world.gridSize; i++) {
+                move(1, 0);
+                sleep(1000 / 16);
+            }
+            for (i = 0; i < y * this.world.gridSize; i++) {
+                move(0, 1);
+                sleep(1000 / 16);
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    collidate(obj) {
+        return true;
+    }
+
+    update () {
+        return true;
+    }
+
+    keyHold(key) {
+        return true;
+    }
+
+    keyPressed(key) {
+        return true;
+    }
+}
+
+
+class StaticNothing {
+    constructor(width, height) {
+        this.height = height;
+        this.width = width;
+    }
+
+    draw(ctx, x, y) {
+        return { x: this.width, y: this.height }
+    }
+}
+
+class StaticBackground {
+    constructor(color, width, height) {
+        this.color = color;
+        this.height = height;
+        this.width = width;
+    }
+
+    draw(ctx, x, y) {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(x, y, this.width, this.height);
+        return { x: this.width, y: this.height }
+    }
+}
+
+class StaticText {
+    constructor(text = "") {
+        //good fonts: Knewave, Coda Caption, Gaegu
+        this.fontSize = 30;
+        this.fontFamily = "Knewave";
+        this.fillStyle = "black";
+        this.textAlign = "left"; //left, center, right
+        this.text = text;
+    }
+
+    draw(ctx, x, y) {
+        ctx.font = this.fontSize + "px " + this.fontFamily;
+        ctx.fillStyle = this.fillStyle;
+        ctx.textAlign = this.textAlign;
+        ctx.textBaseline = "top"; //so the position is like other boxes
+        ctx.fillText(this.text, x, y);
+
+        return { x: ctx.measureText(this.text).width, y: this.fontSize }
+    }
+}
+
+class StaticImg {
+    constructor(url, width, height) {
+        this.url = url;
+        this.img = null;
+        this.height = height;
+        this.width = width;
+    }
+
+    draw(ctx, x, y) {
+        ctx.drawImage(this.img, x, y);
+        return { x: this.width, y: this.height }
+    }
+}
+
+class Sprite {
+    constructor(url, width, height) {
+        this.url = url;
+        this.img = null;
+        this.height = height;
+        this.width = width;
+
+        this.costumes = {};
+        this.n = 0; //count costume
+        this.i = 0; //count pauses
+    }
+
+    set costume(costume) {
+        if (this._costume != costume) {
+            this._costume = costume;
+            this.n = 0;
+        }
+    }
+
+    get costume() {
+        return this._costume
+    }
+
+    draw(ctx, x, y) {
+        let n, yImg, xImg;
+        n = this.costumes[this._costume][this.n];
+        yImg = Math.floor(n / (this.img.width / this.width));
+        xImg = n - (yImg * (this.img.width / this.width))
+
+        ctx.drawImage(this.img, xImg * this.width, yImg * this.height, this.width, this.height, x, y, this.width, this.height);
+
+        if (this.i > 1000 / 16 / this.costumes[this._costume].length) {
+            if (this.costumes[this._costume].length - 1 == this.n) {
+                this.n = 0;
+            } else {
+                this.n = this.n + 1;
+            }
+            this.i = 0;
+        } else {
+            this.i = this.i + 1;
+        }
+
+        return { x: this.width, y: this.height }
+    }
+}
+
+class Figure extends Entity {
+
+    constructor(x, y, width, height, img, type = "figure") {
+        super(x, y, width, height, type);
+
+        this.img = img;
+    }
+
+    draw() {
+        var size = this.img.draw(this.world.ctx, this.x, this.y);
+        super.width = size.x;
+        super.height = size.y;
+    }
+}
+
+const sleep = ms => new Promise(res => setTimeout(res, ms));
---
+++ readme.md
@@ -0,0 +1,202 @@
+# gamelib.js
+
+*gamelib.js is a game libary for german speaking students.*
+
+gamelib.js dient ausschließlich zu Lehrzwecken. Für professionelle Entwicklung in JavaScript gibt es weit besser geeignetere (und komplexere) Bibliotheken. Etwa [phaser.io](https://phaser.io/) oder [melonJS](www.melonjs.org/) (mehr bei [GitHub](https://github.com/collections/game-engines)).
+
+![hello world](screenshot/hello world.png)
+
+
+
+Demos:
+
+* [RPG (Role Playing Game oder "Adventure")](demo-rpg.html)
+* [Plattformer ("Hüpfespiel")](demo-platform.html)
+
+## Vorraussetzungen
+
+Aus Sicherheitsgründen wird dein Webbrowser die Webseite nur anzeigen, wenn sie von einem Server ausgeliefert wird. Bitte ändere diese nicht, sondern verwende einen Server.
+
+Hast du gerade keinen Server zur Hand, kannst du folgendes tun:
+
+1. Öffne Chrome
+2. Installiere das Addon [Web Server for Chrome](https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb)
+3. Starte das Addon und wähle mit `CHOOSE FOLDER` den Ordner mit deinen Dateien aus
+4. Rufe [http://127.0.0.1:8887](http://127.0.0.1:8887/) in deinem Browser auf
+
+## Installation
+
+1. Lade das komplette Repository herunter. 
+2. Erstelle eine index.html mit folgenden Inhalt: 
+
+```html
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>GameLib</title>
+    <link href='css/gamelib.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <game></game>
+
+    <script src="js/gamelib.js"></script>
+    <script>
+        (async function () {
+            //dein Spiel
+        })()
+    </script>
+
+</body>
+
+</html>
+```
+
+Erstelle dein Spiel, dort wie `//dein Spiel` steht.
+
+## Klassen
+
+Im folgenden werden nur dich wichtigen Klassen und Funktionen beschrieben. Alle siehst du bei einem Blick in `js/gamelib.js`
+
+### `World`
+
+Die Welt verwaltet alle Figuren und kümmert sich, um alle Hintergrundaufgaben.
+
+Neue Welt erstellen:
+
+```js
+var world = new World("game", 400, 200);
+```
+
+Dieser Befehl legt eine neue Welt an, welche im html-Element `<game>` erstellt wird. Diese ist 400x200 Pixel groß.
+
+#### Attribute
+
+`background` - hier kannst du eine Darstellung für den Hintergrund festlegen, welche das sein kann findest du unten im Abschnitt Darstellungen
+
+#### Funktionen
+
+`async play()` - Startet das Spiel. Dieser Befehl darf erst aufgerufen werden, wenn alle Figuren hinzugefügt wurden.
+
+`stop()` - Stopt das Spiel.
+
+`getEntity(name)` - Gibt dir eine Figure zurück.
+
+`removeEntity(figure)` - Entfernt die Figure aus der Welt. Das eigentliche Objekt bleibt dabei erhalten.
+
+`removeAllEntities()` - Entfernt alle Figuren. Praktisch, wenn etwa eine neue Welt geladen werden soll.
+
+`addEntity(figure)` - fügt eine Figur hinzu.
+
+### `Figure` (erbt von `Entity`)
+Erstellt eine neue Figur:
+
+```js
+var player = new Figure(50, 110, 64, 64, img);
+```
+
+Erstellt eine Figur, welche an der Position (50, 110) mit der Größe (64, 64) mit der Darstellung `img` .
+
+#### Attribute
+
+`x` - Position auf der x-Achse
+
+`y` - Position auf der y-Achse
+
+`height` - Höhe
+
+`width` - Breite
+
+`img` - Darstellung, siehe Abschnitt Darstellungen
+
+`type` - Typ. Kann frei gewählt werden. Bei Kolliosionen praktisch (Etwa "Gegner" oder "Schatz")
+
+`world` - Hierüber kann eine Figure auf Attribute und Funktionen der beinhaltenden Welt zugreifen.
+
+#### Funktionen
+`move(x,y)`  - Bewegt die Figur um x und y Pixel.
+
+`collidate(obj)` - Diese Funktion wird aufgerufen, wenn etwas mit diesem Objekt kollidiert. Hier können etwa Lebenspunkte abgezogen werden. Am Ende gibst du mit `return` zurück, ob die Kollision zulässig (`true`) oder nicht (`false`) ist. Also darf das Objekt auf der gleichen Position sein? Das kann etwa bei Text oder einem Hintergrundbild sinnvoll sein. Sobald eins der beiden betroffenen Objekte eine Kollision zurückmeldet, wird die Bewegung unterbunden. Beachte bitte, dass diese Funktion mehrmals in der Sekunde aufgerufen wird, wenn die Kollision fortbesteht.
+
+`update()` - Diese Funktion wird aller 16 Milisekunden (also annähernd 60x je Sekunde) aufgerufen. Hier könnte etwa bei Hüpfespielen die Schwerkraft geprüft werden.
+
+### Darstellungen
+
+Du findest im Ordner `img` [viele Bilder](img.md) zum Durchstarten.
+
+
+
+Aktuell gibt es vier Möglichkeiten ein Objekt darzustellen:
+
+`StaticBackground` - Setzt eine Hintergrundfarbe: 
+
+```js
+world.background = new StaticBackground("#558b6e", 400, 200);
+```
+
+Farbe `#558b6e` mit der Größe 400x200 Pixel.
+
+`StaticText` - Schreibt einen beliebigen Text und errechnet selbst die neue Größe des Objektes:
+
+```js
+var img =  new StaticText("Hello World!");
+```
+
+Mit `fontSize `, `fontFamily `, `fillStyle ` und `textAlign ` lässt sich die Schrift anpassen.
+
+Als Schriftarten sind neben den auf den Rechner vorhandenen noch [Coda Caption](https://fonts.google.com/specimen/Coda+Caption),  [Knewave](https://fonts.google.com/specimen/Knewave) und [Gaegu](https://fonts.google.com/specimen/Gaegu) enthalten, welche das Spiel stimmungsvoller wirken lassen.
+
+`StaticImg` - Zeigt ein einzelnes Bild an:
+
+```js
+var img = new StaticImg("img/static-hero.png", 32, 32)
+```
+
+Das Bild wird relativ zur html-Datei mit den Maßen 32x32 Pixel angezeigt.
+
+`Sprite` - Hier wird eine Serie an Bildern durchlaufen, sodass eine Animation entsteht:
+
+```js
+var sprite = new Sprite("img/character-rpg/moderngirl02.png", 32, 48));
+sprite.costumes["down"] = [0, 1, 2, 3];
+sprite.costumes["left"] = [4, 5, 6, 7];
+sprite.costumes["right"] = [8, 9 ,10,11];
+sprite.costumes["up"] = [12,13,14,15];
+sprite.costumes["idle"] = [0];
+sprite.costume = "idle";
+```
+
+Es wird ein Bild relativ zur html-Datei geladen mit den Maßen 32x48 Pixeln. Dabei wird das Bild in diese Bildteile zerlegt. Je nach Bewegung können verschiedene Teilbilder ausgewählt werden. In der letzten Zeile wird die aktuelle Bewegung festgelegt.
+
+Die Bilder werden dabei der Reihe nach gezählt:
+
+![moderngirl02](img/character-rpg/moderngirl02.png)
+
+`StaticNothing ` - Und schon hast du ein unsichtbares Objekt:
+
+```js
+this.img = new StaticNothing(this.width, this.height);
+```
+
+Ändert das eigene Bild, sodass das Objekt nicht mehr zu sehen ist.
+
+### Hilfsfunktionen
+`await sleep(ms)` lässt die aktuelle Aufgabe um `ms` Milisekunden pausieren. Beachte bitte, das dies wirklich nur auf die aktuelle Aufgabe zutrifft. Zudem muss die aufrufende Funktion als `async` markiert sein.
+
+
+## ToDo
+
+* [ ] Gridsystem vollständig implementieren
+* [ ] Helfer für Musik und Töne
+* [ ] Tasteneingaben sollen nur im `canvas` und nicht auf der gesamten Webseite abgefangen werden
+
+
+
+## Lizenz
+
+Bitte beachte, dass die [Grafiken](img.md) und [Schriften](https://fonts.google.com/) von den jeweiligen Urhebern unter verschiedenen Lizenzen veröffentlicht wurden.
+
+Die Bibliothek ist unter [Mozilla Public License 2.0 (MPL)](https://choosealicense.com/licenses/mpl-2.0/) veröffentlicht. Grob gesagt, kannst du alles damit machen, musst aber  die Bibliothek auch mit deinen Änderungen unter den gleichen Bedingungen weitergeben und mich als (Mit-)Urheber nennen. Bei Software reicht ein Link auf dieses Repository. In gedruckten Werken wird im namentliche Nennung wie andere Autoren gebeten.
